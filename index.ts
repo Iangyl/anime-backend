cb169e9bf1f6c326851145d233dc9836be846d3f
Volodymyr Barybin <vohanchib@gmail.com>
2022-11-10 23:51:03 +0200

    connected db

--- index.ts
+++ index.ts
@@ -1,10 +1,23 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 import home from './routes/home';
 
-const app = express();
+dotenv.config();
 
-app.use('/', home);
+async function main() {
+  const port = process.env.PORT || 3001;
+  const password = process.env.MONGO_DB_PWD;
 
-const port = process.env.PORT || 3001;
+  await mongoose.connect(
+    `mongodb+srv://volodymyr_knu:${password}@cluster0.mjj1hej.mongodb.net/?retryWrites=true&w=majority`
+  );
 
-app.listen(port, () => console.log(`App listening to PORT: ${port}`));
+  const app = express();
+
+  app.use('/', home);
+
+  app.listen(port, () => console.log(`App listening to PORT: ${port}`));
+}
+
+main().catch((err) => console.log(err));
